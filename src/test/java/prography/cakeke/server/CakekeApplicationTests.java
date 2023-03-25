03d002abcfb4c5b5fae47e1ae50110de55d15670
govl6113 <govl6113@gmail.com>
2023-03-25 16:34:12 +0900

    chore: second init

---
+++ src/test/java/prography/cakeke/server/CakekeApplicationTests.java
@@ -0,0 +1,13 @@
+package prography.cakeke.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakekeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
