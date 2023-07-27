ec15a70d5817ef9ba2a3288b40e2140e8d9a3514
ympark99 <ympark1230@naver.com>
2023-07-27 11:06:13 +0900

    test(api): 성능테스트용 테스트 api 배포

---
+++ src/main/java/prography/cakeke/server/TestController.java
@@ -0,0 +1,14 @@
+package prography.cakeke.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
